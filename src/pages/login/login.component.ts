4d3da72c38c07115f15026203aa43258f9930fdc
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2025-10-18 20:02:06 -0300

    feat: atualizado layout

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { LoginService } from './login.service';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   selector: 'app-login',
-  imports: [SharedModule],
+  imports: [SharedModule, CardComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   providers: [LoginService],
