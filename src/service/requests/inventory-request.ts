00c332a47d7dc1780aef584005134c8581c457a0
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2025-10-25 16:00:41 -0300

    feat: changing popup inventory

--- src/service/requests/inventory-request.ts
+++ src/service/requests/inventory-request.ts
@@ -2,14 +2,22 @@ import { InventoryTypeEnum } from '../../enums/inventory-type.enum';
 
 export class DoIventoryTransationRequest {
   type!: InventoryTypeEnum;
-  sku_id!: number;
+  skus?: DoInventoryTransactionSkusRequest[];
   inventory_origin_id?: number;
   inventory_destination_id?: number;
-  quantity!: number;
   justification!: string;
 
   parseToRequest(formData: any): DoIventoryTransationRequest {
     Object.assign(this, formData);
+    this.skus = [{
+      sku_id: formData.sku_id,
+      quantity: formData.quantity
+    }];
     return this;
   }
+}
+
+class DoInventoryTransactionSkusRequest {
+  sku_id!: number;
+  quantity!: number;
 }
