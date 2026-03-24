394ce4bec360035286625feb8278f98ffc5aa6a2
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2026-03-24 12:10:15 -0300

    feat: update API_URL to point to production server

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // API_URL: 'https://erp-api-70kd.onrender.com',
-  API_URL: 'http://localhost:8080',
+  API_URL: 'https://erp-api-70kd.onrender.com',
+  // API_URL: 'http://localhost:8080',
 };
